ade14ac4e5d8a620e2a951b47696e9f8f59dbba2
Claudio Lepin <claudio.lepin@uchile.cl>
2020-06-01 14:56:06 -0400

    Minor fixes

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # Google Home's action for Libre Pecado weekly menu
 
-Stage: Development
+Actualmente sólo tiene soporte para idioma en español.
+
+Para solicitar el menú sólo debes invocar el servicio con el siguiente comando por voz:
+
+`Ok Google, Preguntar por menú de Libre Pecado`
+
+Luego de que el asistente te responda, debes preguntar por el menú que deseas saber:
+
+`¿Qué hay mañana para merendar?`
+
+Continúa el dev stage.
--- index.php
+++ index.php
@@ -1,68 +1,67 @@
 <?php
+function checkMenu($data){
+  $message = 'No se han encontrado resultados para tu solicitud';
 
-header('Content-Type: application/json');
-$data = json_decode(file_get_contents('php://input'), true);
+  if (empty($data['queryResult']['parameters']['date-time'])) {
+    $date = $data['queryResult']['parameters']['date-time'];
+    $meal = $data['queryResult']['parameters']['comidas'];
+    $date = date('Y/m/d', strtotime($date));
 
-date_default_timezone_set("America/Santiago");
+    $db_host     = getenv('DB_HOST');
+    $db_schema   = getenv('DB_SCHEMA');
+    $db_username = getenv('DB_USER');
+    $db_pass     = getenv('DB_PASS');
 
-$date = $data['queryResult']['parameters']['date-time'];
-$meal = $data['queryResult']['parameters']['comidas'];
+    $conn = mysqli_connect($db_host, $db_username, $db_pass, $db_schema);
 
-$message = '';
+    if ($conn) {
+      $meal_id = null;
 
-if (empty($data['queryResult']['parameters']['date-time'])) {
-  $message = 'Es necesario que me digas la fecha de la que quieres saber el menú';
-}
-else {
-  $date = date('Y/m/d', strtotime($date));
+      // Get meal id
+      if ($meal != '') {
+        $result = mysqli_query($conn, "SELECT meals_id FROM meals WHERE meals_name = '".$meal."' LIMIT 1");
+        $row    = mysqli_fetch_assoc($result);
 
-  $db_host      = getenv('DB_HOST');
-  $db_schema    = getenv('DB_SCHEMA');
-  $db_username  = getenv('DB_USER');
-  $db_pass      = getenv('DB_PASS');
-
-  $conn = mysqli_connect($db_host, $db_username, $db_pass, $db_schema);
-
-  if (!$conn) {
-    $message  = 'Error de conexión a base de datos';
-  }
-  else {
-    $meal_id    = null;
-
-    // Get meal id
-    if ($meal != '') {
-      $result = mysqli_query($conn, "SELECT meals_id FROM meals WHERE meals_name = '".$meal."' LIMIT 1");
-      $row    = mysqli_fetch_assoc($result);
-
-      if ($row) {
-        $query_meal = ' AND meals.meals_id = '.$row['meals_id'];
-      }
-    }
-
-    $sql = "SELECT menu_date, menu_description, type_name, meals_name FROM menu INNER JOIN meals ON menu.meals_id = meals.meals_id INNER JOIN type ON menu.type_id = type.type_id WHERE menu_deleted = 0 AND menu_date = '".$date."'";
-
-    if ($row['meals_id'] != null) {
-      $sql .= $query_meal;
-    }
-
-    if ($result = mysqli_query($conn, $sql)) {
-      if (mysqli_num_rows($result) > 0) {
-        while ($row = mysqli_fetch_assoc($result)) {
-          $message.= 'Para '.$row['meals_name'].' en el menú '.$row['type_name'].' hay '.$row['menu_description'];
+        if ($row) {
+          $query_meal = ' AND meals.meals_id = '.$row['meals_id'];
         }
       }
-      else {
-        $message = 'No se han encontrado resultados para tu solicitud';
+
+      $sql = "SELECT convert(menu_description USING utf8) as menu_description, type_name, meals_name ";
+      $sql.= "FROM menu INNER JOIN meals ON menu.meals_id = meals.meals_id ";
+      $sql.= "INNER JOIN type ON menu.type_id = type.type_id ";
+      $sql.= "WHERE menu_deleted = 0 AND menu_date = '".$date."'";
+
+      if ($row['meals_id'] != null) {
+        $sql .= $query_meal;
       }
 
-      mysqli_free_result($result);
-    }
-    else {
-      $message = 'Ha ocurrido un error al procesar tu solicitud.';
-    }
+      if ($result = mysqli_query($conn, $sql)) {
+        if (mysqli_num_rows($result) > 0) {
+          while ($row = mysqli_fetch_assoc($result)) {
+            $message.= 'Para '.$row['meals_name'].' en el menú '.$row['type_name'].' hay '.$row['menu_description'];
+          }
+        }
 
-    mysqli_close($conn);
+        mysqli_free_result($result);
+      }
+
+      mysqli_close($conn);
+      return $message;
+    }
+    
+    return 'Ha ocurrido un error al procesar tu solicitud.';
   }
+
+  return 'Es necesario que me digas la fecha de la que quieres saber el menú';
+}
+
+header('Content-Type: application/json');
+date_default_timezone_set("America/Santiago");
+$response = 'No se aceptan estos requests';
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+  $response = checkMenu(json_decode(file_get_contents('php://input'), true));
 }
 
 echo '{
@@ -72,8 +71,8 @@ echo '{
       "richResponse": {
         "items": [
           {
-              "simpleResponse": {
-              "textToSpeech": "'.$message.'"
+            "simpleResponse": {
+              "textToSpeech": "'.$response.'"
             }
           }
         ]
@@ -81,7 +80,4 @@ echo '{
     }
   }
 }'
-
 ?>
-
-
